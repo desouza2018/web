e4ebfd34c2553a4998ba585d95bf6ba668674adc
Adelson de Souza <desouzaadelson2018@outlook.com>
2022-05-02 14:09:32 -0300

    starting the projet

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// npm install para instalar as dependências
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()]
+})
