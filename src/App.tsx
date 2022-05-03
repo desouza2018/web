4e53b0c8329b2f4866ddb0e5d6ef610457d2492c
Adelson de Souza <desouzaadelson2018@outlook.com>
2022-05-02 23:14:18 -0300

    instalação das dependências.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Widget } from "./components/Widget";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  return <Widget/>;
+}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+
+
+
+/** 
+ * interface ButtonProps {
+  text?: string; // A ? torna a propriedade como opcional.
+}
+// as cores podem ser configuradas colocando no lugar do  bg-violet-500, isto: bg-[#8257e6] - isso é um código de cor hexadecimal.
+function Button(props: ButtonProps) {
+  return <button className = "bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{props.text ?? 'Default'}</button>; // Se não for passado o text o valar Default assumirá o lugar.
+}                                                                                          
+                                                                                          
+function App() {                                                                                       
+  return (                                                                              
+    <div className = "flex gap-2">
+      <Button text = "Enviar"/>
+      <Button text = "ok" />
+      <Button />                         
     </div>
-  )
+  ) // Isto é um JSX.
 }
 
 export default App
+*/
+
+
+/**
+ * A function App é um componente react, pois retorna um html.
+ * Por padrão, o nome de um componente ou função inicia-se com 
+ * letra maiúscula: function App(){}.
+ * Os componentes ou função inicia-se com letra maiúscula e as
+ * tags html são inscritas com letra minuscula. 
+ * Exemplo:
+ * function App() {
+ * return <h1>Hello Rocketseat</h1 - Retorno de uma tag html que (JSX)
+ * }
+ * Aprendemos até aqui o core do reactjs, ou seja, sobre os componentes, sobre JSX e 
+ * sobre o props.
+ */
