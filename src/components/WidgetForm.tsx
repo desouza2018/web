436daa07696d55df818550ce89f4c9575cb8545d
Adelson de Souza <desouzaadelson2018@outlook.com>
2022-05-04 20:15:51 -0300

    tudo ok até aqui no WidgetForm

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -3,6 +3,7 @@ import { CloseButton } from "./CloseButton";
 import bugImageUrl from "../assets/bug.svg";
 import ideaImageUrl from "../assets/idea.svg";
 import  thoughtImageUrl from "../assets/thought.svg";
+import { useState } from "react";
 
 const feedbackTypes = {
   BUG: {
@@ -27,8 +28,10 @@ const feedbackTypes = {
     },
   },
 };
+type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -40,7 +43,12 @@ export function WidgetForm() {
         {Object.entries(feedbackTypes).map(([key, value]) => {
           console.log(key, value);
           return (
-            <button>
+            <button 
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gat-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              onClick={() => setFeedbackType(key as FeedbackType)}
+              type="button"
+            >
               <img src={value.image.source} alt={value.image.alt} />
               <span>{value.title}</span>
             </button>
