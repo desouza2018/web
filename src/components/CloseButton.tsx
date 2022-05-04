587a8c83d25183cd00ae6b1da0d654b3882e8e54
Adelson de Souza <desouzaadelson2018@outlook.com>
2022-05-04 19:21:23 -0300

    add svgs no WidgetForm

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,12 @@
+import { X } from "phosphor-react";
+import { Popover } from '@headlessui/react';
+
+export function CloseButton() {
+    return (
+        <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" title="Fechar formulário">
+            <X weight="bold" className="w-4 h-4"/>
+        </Popover.Button>
+    );
+}
+
+// O <X> É UM ICON
