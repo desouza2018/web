e9b5b2468a825164d38fbd771574193a60bc6f91
Adelson de Souza <desouzaadelson2018@outlook.com>
2022-05-06 12:02:56 -0300

    Front end desktop finalizado

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,30 +1,51 @@
 import html2canvas from "html2canvas";
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import { useState } from "react";
 import { Loading } from "../Loading";
 
-export function ScreenshotButton() {
-    const [isTakingScreenshot, setIsTakingScreenshot] = useState(false) 
+interface ScreenshotButtonProps {
+    screenshot: string | null;
+    onScreenshotTook: (screenshot: string | null) => void;
+}
 
-   async function handleTakeScreenshot() {
-       setIsTakingScreenshot(true);
+export function ScreenshotButton({ screenshot, onScreenshotTook } : ScreenshotButtonProps) {
+    const [isTakingScreenshot, setIsTakingScreenshot] = useState(false); 
 
-       const canvas = await html2canvas(document.querySelector('html')!);
-       const base64image = canvas.toDataURL('image/png');
+    async function handleTakeScreenshot() {
+        setIsTakingScreenshot(true);
 
-     console.log(base64image);
+        const canvas = await html2canvas(document.querySelector('html')!);
+        const base64image = canvas.toDataURL('image/png');
 
-     setIsTakingScreenshot(false);
-   }
+        onScreenshotTook(base64image);
+        setIsTakingScreenshot(false);
+    }
+
+    if (screenshot) {
+        return (
+            <button
+                type="button"
+                onClick={() => onScreenshotTook(null)}
+                className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+                style={{
+                    backgroundImage: `url(${screenshot})`,
+                    backgroundPosition: 'right bottom',
+                    backgroundSize: 180
+                }}
+            >
+                <Trash weight="fill" />
+            </button>
+        );
+    }
 
     return (
         <button
-        type="button"
-        onClick={handleTakeScreenshot}
-        className="p-2 bg-zinc800 rounded-md border-transparent hover:bg-zinc-700 transitions-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+            type="button"
+            onClick={handleTakeScreenshot}
+            className="p-2 bg-zinc800 rounded-md border-transparent hover:bg-zinc-700 transitions-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
     > 
-        {isTakingScreenshot ? <Loading /> : <Camera weight="bold" className="w-6 h-6"/> }                          
-    </button>
+            {isTakingScreenshot ? <Loading /> : <Camera weight="bold" className="w-6 h-6"/> }                          
+        </button>
     )
 }
 
